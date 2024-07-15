48809a70885a80b5cf8ff2d7f2e11bb80b08cf59
Landyshlana <135726642+Landyshlana@users.noreply.github.com>
2024-07-15 10:59:10 +0300

    commit1

--- src/js/popover.js
+++ src/js/popover.js
@@ -1,37 +1,43 @@
 export default class Popover {
 	constructor(container) {
 		this.container = container;
-		this.show = false;
-	}
-
-	initPopover() {
-		this.bindToDOM();
-		this.buttonEl = this.container.querySelector('.btn');
-		this.buttonEl.addEventListener('click', (e) => {
-			e.preventDefault();
-			this.showTooltip();
-		});
 	}
 
 	static get markUp() {
 		return `
-	  <div class="popover hidden">
-			  <h3 class="title">Popover title</h3>
-			  <p class="text">And here's some amazing content.It's very engaging.Right?</p>
-  </div>
-  `;
-	}
-
-	get popover() {
-		return this.container.querySelector('.popover');
+        <div class="popover-container hidden">
+            <h3 class="title">Popover title</h3>
+            <p class="text">And here's some amazing content.It's very engaging.Right?</p>
+      </div>
+  <button class="btn">Click to toggle popover</button>`
 	}
 
 	bindToDOM() {
-		this.container.insertAdjacentHTML('afterbegin', this.constructor.markUp);
+		if (!this.container === 0) return;
+		this.container.innerHTML = Popover.markUp;
 	}
 
-	showTooltip() {
-		this.popover.classList.toggle('hidden');
-		this.show = !this.popover.classList.contains('hidden');
+
+	initPopover() {
+		if (!this.container === 0) return;
+		this.bindToDOM();
+		const btn = this.container.querySelector(".btn");
+		btn.addEventListener("click", (e) => {
+			this.onclick(e);
+			this.positionPopover(e);
+
+		});
+	};
+	onclick() {
+		this.tooltip = this.container.querySelector(".popover-container");
+		this.tooltip.classList.toggle('hidden');
 	}
-}
+	positionPopover(e) {
+		const btnclick = e.currentTarget.getBoundingClientRect(e);
+		const popoversize = this.tooltip.getBoundingClientRect()
+		const left = btnclick.left + btnclick.width / 2 - popoversize.width / 2 + window.scrollX;
+		const top = btnclick.top - (popoversize.height) - window.scrollY;
+		this.tooltip.style.left = `${left}px`;
+		this.tooltip.style.top = `${top}px`;
+	};
+};
